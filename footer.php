5230864e7f374ed2b954615dc5f0f3f8a9df5a0a
José Mateus <you@example.com>
2019-09-25 23:12:47 -0300

    Colocando mapa e formulário na página index.php (Trocar token da API do mapa)

--- footer.php
+++ footer.php
@@ -7,15 +7,15 @@
   </footer>
  <?php wp_footer(); ?>
   <!-- Bootstrap core JavaScript -->
-  <script src="vendor/jquery/jquery.min.js"></script>
-  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="<?php bloginfo('template_url')?>/assets/vendor/jquery/jquery.min.js"></script>
+  <script src="<?php bloginfo('template_url')?>/assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
 
   <!-- Plugin JavaScript -->
-  <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
-  <script src="vendor/magnific-popup/jquery.magnific-popup.min.js"></script>
+  <script src="<?php bloginfo('template_url')?>/assets/vendor/jquery-easing/jquery.easing.min.js"></script>
+  <script src="<?php bloginfo('template_url')?>/assets/vendor/magnific-popup/jquery.magnific-popup.min.js"></script>
 
   <!-- Custom scripts for this template -->
-  <script src="js/creative.min.js"></script>
+  <script src="<?php bloginfo('template_url')?>/assets/js/creative.min.js"></script>
 
 </body>
 
--- index.php
+++ index.php
@@ -62,29 +62,169 @@
         </div>
       </div>
     </section>
+<section class="page-section bg-primary map" id="localizacao">
+ <div id="map"></div>
+    <script>
+      var map;
+      function initMap() {
+        map = new google.maps.Map(document.getElementById('map'), {
+          center:{lat:  -5.744509458350604, lng: -35.24621048569423},
+          zoom: 17,
+          styles: [
+                    {
+                        "featureType": "administrative",
+                        "elementType": "labels.text.fill",
+                        "stylers": [
+                            {
+                                "color": "#444444"
+                            }
+                        ]
+                    },
+                    {
+                        "featureType": "landscape",
+                        "elementType": "all",
+                        "stylers": [
+                            {
+                                "color": "#f2f2f2"
+                            }
+                        ]
+                    },
+                    {
+                        "featureType": "poi",
+                        "elementType": "all",
+                        "stylers": [
+                            {
+                                "visibility": "off"
+                            }
+                        ]
+                    },
+                    {
+                        "featureType": "road",
+                        "elementType": "all",
+                        "stylers": [
+                            {
+                                "saturation": -100
+                            },
+                            {
+                                "lightness": 45
+                            }
+                        ]
+                    },
+                    {
+                        "featureType": "road.highway",
+                        "elementType": "all",
+                        "stylers": [
+                            {
+                                "visibility": "simplified"
+                            }
+                        ]
+                    },
+                    {
+                        "featureType": "road.arterial",
+                        "elementType": "labels.icon",
+                        "stylers": [
+                            {
+                                "visibility": "off"
+                            }
+                        ]
+                    },
+                    {
+                        "featureType": "transit",
+                        "elementType": "all",
+                        "stylers": [
+                            {
+                                "visibility": "off"
+                            }
+                        ]
+                    },
+                    {
+                        "featureType": "water",
+                        "elementType": "all",
+                        "stylers": [
+                            {
+                                "color": "#46bcec"
+                            },
+                            {
+                                "visibility": "on"
+                            }
+                        ]
+                    }
+                ]
+          
+        });
+        var marker = new google.maps.Marker({
+            position: {lat:  -5.744509458350604, lng: -35.24621048569423},
+            map:map
+        });
+        attachSecretMessage(marker);
+       function attachSecretMessage(marker) {
+        var infowindow = new google.maps.InfoWindow({
+          content: '<h5>Lavagens a domicílio Silva - Avenida Governador Antônio de Melo e Souza, 2139 - Potengi, Natal - RN</h5>'
+        });
+        marker.addListener('click', function() {
+          infowindow.open(marker.get('map'), marker);
+        });
+      }
+    }
+     
+    </script>
+    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDV9rjWaT7OFIj1Zt0VcMxY29_AhhFXNm4&callback=initMap"
+    async defer></script>
+</section>
+  <!-- Contact Section -->
 
   <!-- Contact Section -->
-  <section class="page-section" id="contact">
+  <section class="page-section bg-primary" id="contact">
     <div class="container">
       <div class="row justify-content-center">
         <div class="col-lg-8 text-center">
           <h2 class="mt-0">Fale conosco</h2>
           <hr class="divider my-4">
-          <p class="text-muted mb-5">Pronto para começar a investir na sua empresa/projeto? </p>
+        
         </div>
       </div>
-      <div class="row">
-        <div class="col-lg-4 ml-auto text-center mb-5 mb-lg-0">
-          <i class="fas fa-phone fa-3x mb-3 text-muted"></i>
-          <div class="cont-num"> (84) 4006-9538</div>
+     <div class="row mx-auto contact">
+
+        
+        <div class="col-lg-6 col-sm-12 ">
+          <div class="title-form mt-lg-0 mt-sm-2 text-flow">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy!</div>
+          <div class="text-flow">
+            <br>
+            <p class="text-flow">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters:</p> 
+            <br>
+          </div>
+          <div class="row mb-3 icones">
+            <div class="col-xs-4 ">
+                <a href="#" target="_blank"><i class="fab fa-facebook-square fa-4x" ></i></a>
+            </div>
+            <div class="col-xs-4 ml-5">
+                <a href="#" target="_blank"><i class="fab fa-instagram fa-4x"></i></a>
+            </div>
+            <div class="col-xs-4 ml-5">
+                <a href="#" target="_blank"><i class="fab fa-whatsapp fa-4x"></i></a>
+            </div>
+            
+          </div>
+            
         </div>
-        <div class="col-lg-4 mr-auto text-center">
-          <i class="fas fa-envelope fa-3x mb-3 text-muted"></i>
-          <!-- Make sure to change the email address in anchor text AND the link below! -->
-          <a class="d-block" href="mailto:contatoitzn@gmail.com">contatoitzn@gmail.com</a>
-        </div>
-      </div>
-    </div>
+
+        <div class="col-lg-6 col-sm-12 form">
+          <form>
+            <div class="form-group text-flow">
+              <label class="" for="formGroupExampleInput">Nome*</label>
+              <input type="text" class="form-control" id="formGroupExampleInput" placeholder="João da Silva">
+            </div>
+            <div class="form-group text-flow">
+              <label class="" for="formGroupExampleInput2">E-mail*</label>
+              <input type="text" class="form-control" id="formGroupExampleInput2" placeholder="João@gmail.com">
+            </div>
+            <div class="form-group text-flow">
+              <label for="exampleFormControlTextarea1">Mensagem*</label>
+              <textarea class="form-control" id="exampleFormControlTextarea1" placeholder="Sua mensagem" rows="3"></textarea>
+            </div>
+          </form>
+
+     </div>
   </section>
 
   <?php get_footer(); ?>
